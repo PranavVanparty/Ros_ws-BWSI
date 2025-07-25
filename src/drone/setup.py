3e4cbdda3ee1409992eb2a65591bb35fc49ccaaa
PranvVanparty <pvanparty@gmail.com>
2025-07-25 17:26:12 +0000

    fixed error

--- src/drone/setup.py
+++ src/drone/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "pathfinder = drone.pathfinder:main"
+            "pathfinder = drone.pathfinder:main",
             "april_tags = drone.AprilTags:main",
         ],
     },
